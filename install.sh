7de5ffd5593e8ed9b1d357ff5a934ab15be00785
Dan Ivy <danivy4@gmail.com>
2019-02-23 14:57:11 -0600

    Improve installer

--- install.sh
+++ install.sh
@@ -1,46 +1,49 @@
 #!/bin/bash
 
 # settings
-install_nanorc_to_all='yes'
-install_vimrc_to_all='yes'
-
-bashrc_source=bashrc
-bashrc_dist=~/.bashrc
-gitconfig_source=gitconfig
-gitconfig_dist=~/.gitconfig
-nanorc_source=nanorc
-nanorc_dist=~/.nanorc
-nanorc_dist_all=/etc/nanorc
-vimrc_source=vimrc
-vimrc_dist=~/.vimrc
-vimrc_dist_all=/etc/vim/vimrc
+BASHRC_SOURCE=bashrc
+BASHRC_DESTS=(~/.bashrc)
+GITCONFIG_SOURCE=gitconfig
+GITCONFIG_DESTS=(~/.gitconfig)
+NANORC_SOURCE=nanorc
+NANORC_DESTS=(~/.nanorc /etc/nanorc /etc/nano/nanorc)
+VIMRC_SOURCE=vimrc
+VIMRC_DESTS=(~/.vimrc /etc/vimrc /etc/vim/vimrc)
 
 # functions
 
+function lookup {
+  local x="$1"
+  local arr=( "${@:2}" )
+  local output=-1
+  for index in "${!arr[@]}"; do
+    if [[ "${x}" = "${arr[$index]}" ]]; then
+      output=$index
+      break
+    fi
+  done
+  return $output
+}
+
 function is_yes {
-  if [ "${1,,}" == 'y' ] || [ "${1,,}" == "yes" ]; then
-    return 0
-  else
-    return 1
-  fi
+  local input="$1"
+  return $([ "${input,,}" == 'y' ] || [ "${input,,}" == "yes" ])
 }
 
 function is_no {
-  if [ "${1,,}" == 'n' ] || [ "${1,,}" == "no" ]; then
-    return 0
-  else
-    return 1
-  fi
+  local input="$1"
+  return $([ "${input,,}" == 'n' ] || [ "${input,,}" == "no" ])
 }
 
 function confirm {
-  msg="$1"
+  local msg="$1"
   if [ "$msg" != "" ]; then
     msg="$msg "
   fi
-  output=-1
+  local output=-1
   while [ $output -eq -1 ]; do
     echo -n "$msg(y/n) "
+    local input
     read input
     if is_yes "$input"; then
       output=0
@@ -51,18 +54,50 @@ function confirm {
   return $output
 }
 
+function select_option {
+  local msg="$1"
+  local options=( "${@:2}" )
+  if [ ${#options[@]} -eq 0 ]; then
+    (>&2 echo "select_option requires at least 2 arguments")
+    exit 1
+  elif [ ${#options[@]} -eq 1 ]; then
+
+    return 0
+  fi
+  if [ "$msg" != "" ]; then
+    echo "$msg"
+  fi
+  local num=0
+  local selection=""
+  select selection in "${options[@]}"; do
+    case "$selection" in
+      "")
+        echo "Invalid option"
+        ;;
+      *)
+        break
+        ;;
+    esac
+  done
+  lookup "$selection" "${options[@]}"
+  return $!
+}
+
+function install {
+  local source="$1"
+  local dest_options=( "${@:2}" )
+  if confirm "Install $source?"; then
+    select_option "Select where to install $source:" "${dest_options[@]}"
+    local dest="${dest_options[$?]}"
+    echo "Copying $source to $dest"
+    cp $source $dest
+  fi
+}
+
+
 # installation
-cp $bashrc_source $bashrc_dist
-echo "Remember to call 'source ~/.bashrc'!"
-cp $gitconfig_source $gitconfig_dist
-if confirm "Install nanorc for all users?"; then
-  sudo cp $nanorc_source $nanorc_dist_all
-else
-  cp $nanorc_source $nanorc_dist
-fi
-if confirm "Install vimrc for all users?"; then
-  sudo cp $vimrc_source $vimrc_dist_all
-else
-  cp $vimrc_source $vimrc_dist
-fi
+install "$BASHRC_SOURCE" "${BASHRC_DESTS[@]}"
+install "$GITCONFIG_SOURCE" "${GITCONFIG_DESTS[@]}"
+install "$NANORC_SOURCE" "${NANORC_DESTS[@]}"
+install "$VIMRC_SOURCE" "${VIMRC_DESTS[@]}"
 
