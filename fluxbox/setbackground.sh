c87c0d9731564131f9d06dd4e8e752369fba4fe6
Dan Ivy <danivy4@gmail.com>
2019-12-26 19:35:21 -0600

    Support multiple background file types

--- fluxbox/setbackground.sh
+++ fluxbox/setbackground.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
-fbsetbg -a ~/.fluxbox/backgrounds/background.jpg
+set -e
+
+paths=(\
+  ~/.fluxbox/backgrounds/background.jpg \
+  ~/.fluxbox/backgrounds/background.png \
+)
+
+function main {
+  for path in "$@"; do
+    if [[ -f "$path" ]]; then
+      echo "Setting background to $path..."
+      fbsetbg -a "$path"
+      return 0
+    fi
+  done
+}
+
+main "${paths[@]}"
