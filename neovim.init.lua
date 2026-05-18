015c1c061a8e2ba022699023866e7a4279349739
Dan Ivy <danivy4@gmail.com>
2026-05-17 23:22:50 -0500

    Add more neovim language servers

--- neovim.init.lua
+++ neovim.init.lua
@@ -2,6 +2,10 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+-- Disable mouse popup in menu
+vim.cmd('aunmenu PopUp.How-to\\ disable\\ mouse')
+vim.cmd('aunmenu PopUp.-2-')
+
 -- Packages
 vim.pack.add({
   -- Language server protocol (LSP) configs for many languages
@@ -10,18 +14,9 @@ vim.pack.add({
   { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
   -- File tree
   { src = 'https://github.com/nvim-tree/nvim-tree.lua' },
+  -- Color scheme
+  { src = 'https://github.com/srcery-colors/srcery-vim' },
 })
-
--- Setup nvim-tree
-require("nvim-tree").setup()
-
-vim.o.number = true -- Show line numbers
-vim.o.shiftwidth = 2 -- Use 2 spaces for tabs before first non-space char
-vim.o.tabstop = 2 -- Use 2 spaces for tabs after first non-space char
-vim.o.smartindent = true -- Auto indent lines
-vim.o.cursorline = true -- Highlight the current line
-vim.o.expandtab = true -- Use spaces instead of tabs
-vim.o.termguicolors = true -- Enable 24-bit colors
 
 -- Autocomplete
 vim.opt.completeopt = {
@@ -30,18 +25,44 @@ vim.opt.completeopt = {
   "popup", -- Show extra info about completion options
 }
 
+-- Diagnostics
+vim.diagnostic.config({
+  virtual_text = true,       -- Enable inline messages
+  signs = false,             -- Enable gutter signs
+  underline = true,          -- Enable underlines for errors
+  update_in_insert = false,  -- Disable updates while typing
+  severity_sort = true,      -- Sort diagnostics by severity
+})
+
 -- LSP
 
 -- Good list of LSPs here: https://wiki.archlinux.org/title/Language_Server_Protocol
 -- Available configs here: https://github.com/neovim/nvim-lspconfig/tree/master/lsp
 
 -- List of LSPs to enable
-lsps = {"bashls", "pyright"}
+lsps = {
+  "bashls",
+  "deno", -- JavaScript and TypeScript
+  "dockerls",
+  "gopls",
+  "lua_ls",
+  "pyright",
+  "rumdl", -- Markdown
+  "systemd_lsp",
+  "terraformls",
+}
 
 -- For each LSP config, enable autocomplete in it
 for i, lsp in ipairs(lsps) do
   vim.lsp.config(lsp, {
     on_attach = function(client, bufnr)
+      -- Any printable char triggers autocomplete
+      local chars = {}
+      for i = 32, 126 do
+        table.insert(chars, string.char(i))
+      end
+      client.server_capabilities.completionProvider.triggerCharacters = chars
+
       vim.lsp.completion.enable(true, client.id, bufnr, {
         autotrigger = true,
         convert = function(item)
@@ -53,4 +74,22 @@ for i, lsp in ipairs(lsps) do
   })
   vim.lsp.enable(lsp)
 end
+
+-- Note: check lsp health with :checkhealth vim.lsp
+
+-- Color scheme
+vim.g.srcery_bold = 0
+vim.g.srcery_italic = 0
+vim.cmd.colorscheme('srcery')
+
+-- Tree browser
+require("nvim-tree").setup()
+
+vim.o.number = true -- Show line numbers
+vim.o.shiftwidth = 2 -- Use 2 spaces for tabs before first non-space char
+vim.o.tabstop = 2 -- Use 2 spaces for tabs after first non-space char
+vim.o.smartindent = true -- Auto indent lines
+vim.o.cursorline = true -- Highlight the current line
+vim.o.expandtab = true -- Use spaces instead of tabs
+vim.o.termguicolors = true -- Enable 24-bit colors
 
